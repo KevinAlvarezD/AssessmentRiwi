3af64e4623e859f5ba9640769b82672ee9a5c33d
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-11-14 21:19:05 -0500

    Feat: Create modelUser

---
+++ Models/User.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentRiwi.Models;
+
+[Table("users")]
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+    [Column("last_name")]
+    public string LastName { get; set; }
+    [Column("address")]
+    public string Address { get; set; }
+    [Column("password")]
+    public string Password { get; set; }
+    [Column("email")]
+    public string Email { get; set; }
+    [Column("phone")]
+    public string Phone { get; set; }
+    [Column("role")]
+    public string Role { get; set; }
+
+    public User()
+    {  
+    }
+
+
+}
