81a4d44be3a3dcd98b5f89090fc116ee81d65b1f
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-11-14 21:22:28 -0500

    Feat: Create User services with hash and token JWT

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace AssessmentRiwi.Data;
 public class ApplicationDbContext : DbContext
 {
    
-    // public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
      
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
---
+++ Helpers/JwtTokenHelper.cs
@@ -0,0 +1,31 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StoreAppi.Helpers
+{
+    public static class JwtTokenHelper
+    {
+        public static string GenerateToken(string name,  string role, string secretKey)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, name ),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
+
